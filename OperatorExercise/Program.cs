e60c4472f8c71ba872ed9536a4ee36ef1731f208
Spencer Hansen <spencerhansen@gmail.com>
2022-12-14 18:07:49 -0700

    operator exercises

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine(a + "/" + b + " is " + quotient + " remainder " + remainder);
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("please enter the radius or your circle:");
+
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine(AreaOfCircle(radius));
+
+            //var i = 3;
+            //var j = 4;
+            //var k = ++i * j++;
+
+            //Console.WriteLine(k);   
+
+            
         }
+
+        public static double AreaOfCircle(double radius)
+        {
+            double area = Math.PI * Math.Pow( radius, 2);
+
+            return area;    
+        }
+            
+
+
+
     }
 }
